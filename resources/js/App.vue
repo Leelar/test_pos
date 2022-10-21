19e1e66a277b178d816aa0eb88ab5e43270618e7
leelar <leelarthor@gmail.com>
2022-10-21 13:32:33 +0700

    first commit by Leelar

---
+++ resources/js/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TestPosApp',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
